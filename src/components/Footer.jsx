a418a5b503873917138517a9973032dd27f4f9b6
HalaAbuShalbak <halaj.abushalbak@gmail.com>
2025-05-29 19:57:44 +0300

    landing page almost done , navbar , sections , footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+  <footer className="flex md:flex-row flex-col bottom-0 text-black w-full justify-evenly items-start md:items-center border-t-2 px-4 py-6 gap-4 md:gap-0">
+  <div className="flex flex-col gap-2 text-sm text-start">
+    <strong>Contact</strong>
+    <a className="hover:animate-pulse underline" href="tel:00962798967357">
+      +962798967357
+    </a>
+    <a
+      className="hover:animate-pulse underline"
+      href="https://www.linkedin.com/in/halaabushalbak/"
+    >
+      LinkedIn: halaabushalbak
+    </a>
+    <a
+      className="hover:animate-pulse underline"
+      href="mailto:halaj.abushalbak@gmail.com"
+    >
+      Email: halaj.abushalbak@gmail.com
+    </a>
+  </div>
+
+  <div className="flex flex-col gap-2 text-sm text-start">
+    <strong>Useful Links</strong>
+    <a className="hover:animate-pulse underline" href="#about">About Us</a>
+    <a className="hover:animate-pulse underline" href="#categories">Categories</a>
+    <a className="hover:animate-pulse underline" href="#newArr">New Arrivals</a>
+  </div>
+
+  <div className="flex flex-col gap-2 text-sm text-start">
+    <strong>Built Using</strong>
+    <p>React</p>
+    <p>Vite</p>
+  </div>
+</footer>
+
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+     
+      <div id="categories">
+
+<h3>CATEGORIES</h3>
+      </div>
+      <div id="about">
+
+<h3>ABOUT SECTION</h3>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam tenetur maxime quod, dolorum ullam perspiciatis, qui aliquid sint natus quam corporis laudantium temporibus soluta in, iure quas asperiores culpa harum.
+Dicta ad, sequi, repellendus iste quaerat ipsam veritatis libero consequuntur facilis distinctio enim voluptatum labore provident, tenetur eaque quo accusamus architecto laboriosam blanditiis. Illum repellat eveniet temporibus ad a cum.
+Libero aliquam repudiandae facere. Praesentium, natus! Magni assumenda suscipit autem architecto doloribus illum rerum nam nobis totam hic perferendis et recusandae necessitatibus, fuga voluptatem quo odio sapiente! Asperiores, blanditiis accusantium?
+Fugiat excepturi hic placeat iure veniam, ad quam voluptas quod delectus dolores sed ut at earum culpa facere perspiciatis necessitatibus ratione blanditiis expedita modi ipsa eligendi. Rem delectus aliquam similique!
+Perferendis dolorem debitis natus molestiae consectetur corporis ratione officiis eius tempora aliquid exercitationem eum, veritatis minima iste. Excepturi sequi labore ipsa deleniti modi ipsum, amet et molestiae asperiores sint tempora.
+      </div>
+      <div id="contact">
+
+CONT
+      </div>
+      <div id="newArr">
+
+NEW
+      </div>
+      <Footer/>
+    </>
+  );
+};
+
+export default Home;
